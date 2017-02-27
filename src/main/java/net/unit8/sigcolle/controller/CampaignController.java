842a9678d3b9a38dd7609c65f66ff86bed845e8e
140ka93 <trumpet_ponyo_0903@yahoo.co.jp>
2017-02-27 15:57:21 +0900

    Campaign登録できるようになりました。

--- src/main/java/net/unit8/sigcolle/controller/CampaignController.java
+++ src/main/java/net/unit8/sigcolle/controller/CampaignController.java
@@ -109,12 +109,15 @@ public class CampaignController {
         Campaign model = new Campaign();
         model.setStatement(processor.markdownToHtml(form.getStatement()));
         model.setCreateUserId(principal.getUserId());
+        model.setTitle(form.getTitle());
+        model.setGoal(Long.parseLong(form.getGoal()));
 
-        CampaignDao campaignDao = domaProvider.getDao(CampaignDao.class);
         // TODO Databaseに登録する
+        CampaignDao campaignDao = domaProvider.getDao(CampaignDao.class);
+        campaignDao.insert(model);
 
         HttpResponse response = redirect("/campaign/" + model.getCampaignId(), SEE_OTHER);
-        response.setFlash(new Flash<>(""/* TODO: キャンペーンが新規作成できた旨のメッセージを生成する */));
+        response.setFlash(new Flash<>("success"/* TODO: キャンペーンが新規作成できた旨のメッセージを生成する */));
 
         return response;
     }
